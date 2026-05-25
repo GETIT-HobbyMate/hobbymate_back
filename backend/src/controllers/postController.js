7c935d656e316b24bfc80ed44f704537ec6e0e7d
bradyhyj <bradyhyj@gmail.com>
2026-05-25 20:47:04 +0900

    feat: 모집 게시글 리스트 조회
    - 모집 게시글 리스트 조회 기능 완성
    - TODO: 나중에 mockup data로 테스트해 볼 것

---
+++ backend/src/controllers/postController.js
@@ -0,0 +1,44 @@
+import pool from '../db.js'
+
+// 모집 게시글 리스트 조회
+// TODO: 나중에 mockup data로 테스트해볼 것
+export const getAllPosts = async (req, res, next) => {
+  try {
+    // gemini 피셜 group_concat이 태그들을 하나의 문자열로 묶어준다고 하는데..
+    // 이상하면 나중에 이쪽을 건드려보면 될듯
+
+    const sql = `
+    SELECT p.id AS postId,
+           p.title AS title,
+           p.current_capacity AS currentCapacity,
+           p.max_capacity AS maxCapacity,
+           p.meeting_time AS meetingTime,
+           IF(p.current_capacity >= p.max_capacity, true, false) AS isFulled,
+           GROUP_CONCAT(t.tag_name) AS tags
+    FROM posts p
+    LEFT JOIN post_tags t ON p.id = t.post_id
+    GROUP BY p.id
+    `;
+    const [rows] = await pool.query(sql);
+
+
+    // formattedPosts: 출력 형식 조정
+    const formattedPosts = rows.map(row => ({
+      ...row,
+      isFulled: Boolean(row.isFulled),
+      tags: row.tags ? row.tags.split(',') : []
+    }));
+    
+
+
+    res.status(200).json({
+      "success": true,
+      "message": "게시글 목록을 불러왔습니다.",
+      "data": {
+        "posts": formattedPosts
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
